a0e60a0f3d3fc58fcdb9762206c7d668907136c7
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2020-03-29 13:34:35 +0200

    First commit and first test

---
+++ Pyndemic/units.py
@@ -0,0 +1,19 @@
+"""
+Basic units used in the pandemic modelling
+"""
+
+from collections import namedtuple
+
+class HouseHold:
+    Members = namedtuple('Members', 'kids adults retirees')
+
+    def __init__(self, kids, adults, retirees):
+        self.members = Members(kids=kids, adults=adults, retirees=retirees)
+        self.exposed = Members(kids=0, adults=0, retirees=0)
+        self.infectious = Members(kids=0, adults=0, retirees=0)
+        self.recovered = Members(kids=0, adults=0, retirees=0)
+
+    def infect(self, kids, adults, retirees):
+        infected_kids = self.infected.kids + kids
+        infected_adults = self.infected.adults + adults
+        infected_retirees = 
---
+++ tests/test_units.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestHouseHould(unittest.TestCase):
+    def test_fake(self):
+        self.assertEqual(1, 1)
+
+if __name__ == "__main__":
+    unittest.main()
+
