f5fc1c1d54f1f99e127e7dc7aef1354df9d47573
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2020-03-29 16:09:13 +0200

    More thorough testing

--- tests/test_units.py
+++ tests/test_units.py
@@ -25,12 +25,57 @@ class TestHouseHold(unittest.TestCase):
         self.assertEqual(h.R.adults, 0)
         self.assertEqual(h.R.retirees, 0)
 
+        self.assertEqual(h.D.kids, 0)
+        self.assertEqual(h.D.adults, 0)
+        self.assertEqual(h.D.retirees, 0)
+
     def test_infecting(self):
-        h = HouseHold(2, 2, 0)
+        h = HouseHold(2, 2, 1)
         h.infect(1, 0, 0)
+
+        self.assertEqual(h.S.kids, 1)
+        self.assertEqual(h.S.adults, 2)
+        self.assertEqual(h.S.retirees, 1)
+
         self.assertEqual(h.E.kids, 1)
         self.assertEqual(h.E.adults, 0)
         self.assertEqual(h.E.retirees, 0)
+
+        self.assertEqual(h.I.kids, 0)
+        self.assertEqual(h.I.adults, 0)
+        self.assertEqual(h.I.retirees, 0)
+
+        self.assertEqual(h.R.kids, 0)
+        self.assertEqual(h.R.adults, 0)
+        self.assertEqual(h.R.retirees, 0)
+
+        h.infect(0, 1, 0)
+
+        self.assertEqual(h.S.kids, 1)
+        self.assertEqual(h.S.adults, 1)
+        self.assertEqual(h.S.retirees, 1)
+
+        self.assertEqual(h.E.kids, 1)
+        self.assertEqual(h.E.adults, 1)
+        self.assertEqual(h.E.retirees, 0)
+
+        self.assertEqual(h.I.kids, 0)
+        self.assertEqual(h.I.adults, 0)
+        self.assertEqual(h.I.retirees, 0)
+
+        self.assertEqual(h.R.kids, 0)
+        self.assertEqual(h.R.adults, 0)
+        self.assertEqual(h.R.retirees, 0)
+
+        h.infect(0, 0, 1)
+
+        self.assertEqual(h.S.kids, 1)
+        self.assertEqual(h.S.adults, 1)
+        self.assertEqual(h.S.retirees, 0)
+
+        self.assertEqual(h.E.kids, 1)
+        self.assertEqual(h.E.adults, 1)
+        self.assertEqual(h.E.retirees, 1)
 
         self.assertEqual(h.I.kids, 0)
         self.assertEqual(h.I.adults, 0)
